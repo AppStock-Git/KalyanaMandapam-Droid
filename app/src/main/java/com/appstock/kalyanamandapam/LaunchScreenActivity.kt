58122fbc6d016bc0b55dfcc8357349e2624af52f
vramachandran-pk <vramachandran@prokarma.com>
2018-01-05 13:11:49 +0530

    adding project files

---
+++ app/src/main/java/com/appstock/kalyanamandapam/LaunchScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.appstock.kalyanamandapam
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
